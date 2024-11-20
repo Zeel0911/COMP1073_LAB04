a76fdd5ab1122d5ea86b5040cd388a0fdb6f77c4
Zeel0911 <156478385+Zeel0911@users.noreply.github.com>
2024-11-19 19:42:31 -0500

    Update json.js

--- lesson-10/js/json.js
+++ lesson-10/js/json.js
@@ -1,10 +1,14 @@
 /* STEP 2: Reference the HEADER and the SECTION elements with variables */
+const header = document.querySelector('header');
+const section = document.querySelector('section');
 
 
 // STEP 3a: Create the asynchronous function populate()
+async function populate() {
 
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4: Store the URL of a JSON file in a variable */
+    const requestURL = './js/ice-cream.json'; 
     
     // STEP 5: Use the new URL to create a new request object
     
